319fd3910dad8d678c3bcaa9a32a77a50fa5f4dc
Hudson Hadley <149119534+hudsonhadley@users.noreply.github.com>
2023-11-05 17:42:23 -0500

    Update PresetValues.java

--- PresetValues.java
+++ PresetValues.java
@@ -1,13 +1,7 @@
-import java.awt.*;
-
 public class PresetValues
 {
-	public final int TOP_OF_DINO = 578;
-	public final int RIGHT_OF_DINO = 123;
-	public final int BOTTOM_OF_DINO = 619;
-	public final int LEFT_OF_DINO = 57;
-
-public final Point GAME_OVER = new Point(551, 450);
-
-public final int GAME_OVER_COLOR = -5460820;
+	public static final int TOP_OF_DINO = 578;
+	public static final int RIGHT_OF_DINO = 122;
+	public static final int BOTTOM_OF_DINO = 618;
+	public static final int TOP_OF_RETRY = 536;
 }
